84741eead4e556a57dbbe4fce075708c8979f269
bjm2020 <bjm2020@gmail.com>
2017-06-03 19:48:49 -0500

    InfoWindow Image Source bug

--- js/main.js
+++ js/main.js
@@ -6,21 +6,45 @@
   this.street = ko.observable(data.street);
   this.category = ko.observable(data.category);
   this.categoryID = data.categoryID;
-  this.foursquareID = data.foursqureID;
+  this.foursquareID = data.foursquareID;
   this.iconLink =  data.iconLink;
   this.location = data.location;
+  this.rating = ko.observable(data.rating);
+  this.ratingColor =  ko.observable(data.ratingColor);
+  this.latestTip = ko.observable(data.latestTip);
+  this.bestPhotoLink = ko.observable(data.bestPhotoLink);
+  this.photos = ko.observable(data.photos);
 
   this.marker;
+
+  this.bestPhotoLink.subscribe(function(newValue) {
+       if (window.console && console.log) {
+           console.log(++this.triggeredCount, "firstName triggered with new value", newValue);
+       }
+     });
+
+
+
   //this.markerID = 0;
 
-  this.address = this.street + " " + this.city;
+  this.address = this.street() + " " + this.city();
 
+}
+
+Location.prototype.addExtraData = function(data) {
+  this.rating(data.rating);
+  this.ratingColor(data.ratingColor);
+  this.latestTip(data.latestTip);
+  this.photos(data.photos);
+  this.bestPhotoLink(data.bestPhotoLink);
+//  console.log(this.bestPhotoLink());
 }
 
   var ViewModel = function() {
      var self = this;
-     this.locationList = ko.observableArray([]);
 
+     this.locationList = ko.observableArray([]);
+     self.currentLocation = ko.observable(self.locationList()[0]);
      self.getFourSquareLocations = function() {
        var clientID = "PGZR20FMKL3MDGHKVNZFGE1N5AK02NPUCUIVQ5YFALGYSV1M";
        var clientSecret = "5LM1W4RDO2BUO24UFCLDEXFQM0DJULOMDVTRAWXC1PMSRGQQ";
@@ -35,7 +59,8 @@
              var venues = data.response.venues;
              for (var i = 0; i < venues.length; i++) {
                var venue = venues[i];
-
+               console.log(venue.id);
+            //  console.log(venue.name);
              var venueData = {
                name: venue.name,
                street: venue.location.address,
@@ -45,18 +70,71 @@
                foursquareID: venue.id,
                location: {lat: venue.location.lat, lng: venue.location.lng},
                iconLink: venue.categories[0].icon.prefix+"bg_32"+venue.categories[0].icon.suffix
-             }
-
+             } //venue data
+          //   console.log(venueData);
              self.locationList.push(new Location(venueData));
 
-             }
-             console.log(self.locationList().length);
-             startApp();
+           } //for loop
+            // console.log(self.locationList().length);
+//startApp();
+          } //success
 
-           }
-         });
-     };
+        }).done(function() {
+          console.log("start 2nd ajax");
+          console.log(self.locationList()[0].foursquareID);
+          console.log(self.locationList().length);
+           for (var i = 0; i < self.locationList().length;i++) {
+            console.log("start loop");
+            var index = i;
+           $.ajax({
+             url: 'https://api.foursquare.com/v2/venues/'+self.locationList()[i].foursquareID+'?&client_id='+clientID+'&client_secret='+clientSecret+'&v=20160108',
+             dataType: "json",
+             success: function(data) {
+               var extras = data.response.venue;
+               var latestTip;
+              console.log(extras.rating);
+              var rating;
+              var ratingColor;
+              var bestPhotoLink;
+              if(typeof extras.rating !== "undefined") {
+                rating = extras.rating;
+                ratingColor = extras.ratingColor;
+              }
+              else {
+                rating = "No Rating Available";
+                ratingColor = "none";
+              }
+               if(extras.tips.count > 0) {
+                 latestTip = extras.tips.groups[0].items[0];
+               } //if
+               else {latestTip = "no tips avaialable from this location";}
+               if(typeof extras.bestPhoto != "undefined") {
+                 bestPhotoLink = extras.bestPhoto.prefix + "100x100" + extras.bestPhoto.suffix;
+               }
+               else {
+                 bestPhotoLink = "http://via.placeholder.com/100x100";
+               }
+               //else
+               //console.log("ratings" + extras.price.rating);
+               var extraData = {
+               bestPhotoLink: bestPhotoLink,
+               rating: rating,
+               ratingColor: ratingColor,
+               photos: extras.photos,
+               latestTip: latestTip
+           } //extraData
+            self.locationList()[index].addExtraData(extraData);
+         } //success
+       }); //ajax
 
+         console.log(self.locationList()[i]);
+
+       } //for loop
+startApp();
+        }); //ajax
+
+
+}
 self.getFourSquareLocations();
 
 var map;
@@ -108,6 +186,7 @@ function populateInfoWindow(marker, infowindow, content) {
   if(infowindow.marker != marker) {
     infowindow.marker = marker;
     infowindow.setContent($('#info-window-test').html());
+  //  console.log(self.currentLocation());
   //  infowindow.setContent(content);
     infowindow.open(map, marker);
 
@@ -135,9 +214,13 @@ function hideLocations(id) {
   }
 }
 
-this.currentLocation = ko.observable(self.locationList()[0]);
 
 
+self.currentLocation.subscribe(function(newValue) {
+     if (window.console && console.log) {
+         console.log(++this.triggeredCount, "Current Location triggered with new value", newValue);
+     }
+   });
 //Stores Value of Drop Down List Category Filter.
 this.selectedCategory = ko.observable();
 
@@ -182,12 +265,15 @@ this.selectionChanged = function() {
 
 
 this.setLocation = function(param) {
-
+/*
+  console.log(param);
   //TODO: do something with location
+
   if(typeof self.currentLocation() === "undefined") {
     self.currentLocation(param);
   }
 else {
+    console.log(self.currentLocation());
   //console.log(param);
   //console.log(locationList()[i].name + " " + param);
   for(var i=0; i < self.locationList().length;i++){
@@ -198,6 +284,10 @@ else {
     }
   }
 }
+*/
+//console.log(self.currentLocation().bestPhotoLink);
+self.currentLocation(param);
+console.log(self.currentLocation().name)
   for(var i=0;i<markers.length;i++){
   //  console.log(self.currentLocation().markerID);
   //  console.log(markers[i].id);
@@ -208,15 +298,18 @@ else {
   }
   console.log("setLoc");
 };
+
 };
 
 var model = new ViewModel();
 
 ko.applyBindings(model);
+
+//startApp();
 //console.log(locationList().length);
 
 
-var startApp = function() {
+function startApp() {
 //  model = new ViewModel();
  //ko.applyBindings(model);
   model.initMap();
